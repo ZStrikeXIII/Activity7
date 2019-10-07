43582083c9a1e4bfafa188024eef66d49bb064c1
Jackson Turner <jbturner6375@eagle.fgcu.edu>
2019-10-07 18:02:34 -0400

    Initial commit

---
+++ src/bikeproject/MountainParts.java
@@ -0,0 +1,14 @@
+package bikeproject;
+
+public interface MountainParts {
+
+  public final String TERRAIN = "off_road";
+
+  public String getSuspension();
+
+  public void setSuspension(String newValue);
+
+  public String getType();
+
+  public void setType(String newValue);
+}
---
+++ src/bikeproject/RoadParts.java
@@ -0,0 +1,14 @@
+package bikeproject;
+
+public interface RoadParts {
+
+  public final String TERRAIN = "track_racing";
+
+  public int getTyreWidth();
+
+  public void setTyreWidth(int newValue);
+
+  public int getPostHeight();
+
+  public void setPostHeight(int newValue);
+}
